7ba59e52072bb56fe6aa159fd8f66ab10f7ed7fd
rails <rails@example.com>
2020-08-05 00:25:27 +0530

    task done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "questions#index"
+  resources :questions
+  resources :users do
+    member do
+      post 'follow'
+    end
+  end
+  resources :topics do
+    member do
+      post 'follow'
+    end
+  end
 end
